791089c717b25e3521db683015def27562a0015a
Kevin van der Vlist <kevin@kevinvandervlist.nl>
2012-01-05 14:54:04 +0100

    Extended QLMode config.

--- .emacs.d/ql-mode.el
+++ .emacs.d/ql-mode.el
@@ -6,9 +6,11 @@
 (define-generic-mode 
   'ql-mode                         ;; name of the mode to create
   '("#")                           ;; comments start with '#'
-  '("LESSON" "TITLE" "PURPOSE" "GOALS" "KEYWORDS" "OVERVIEW" ;; Keywords
-		"DATA" "THEORY" "IMAGE" "AUDIO" "DISK" "TEXT" "QUESTION"
-		"TYPE" "JSEMU" "TEXT" "ANSWER" "CORRECT" "EXPLANATION" "ACHIEVED")
+	'("LESSON" "TITLE" "PURPOSE" "GOALS" "KEYWORDS" "DATA" "PARTS" 
+		"TEXT" "IMAGE" "AUDIO" "OVERVIEW" "ACHIEVED" "QUESTION" "TYPE" 
+		"ANSWER" "CORRECT" "EXPLANATION" "THEORY" "DISK" "SHELL" "TCMD"
+		"TUSER" "TSEP" "THOST" "TPATH" "TPROMPT" "TCOMMAND" "TRESULT" "TLINE"
+		)
   '(("" . 'font-lock-operator)     ;; '=' is an operator
     ("OPEN" | "MC" | "JSEMU" . 'font-lock-builtin))     ;; ';' is a a built-in 
   '("\\.ql$")                      ;; files for which to activate this mode 
