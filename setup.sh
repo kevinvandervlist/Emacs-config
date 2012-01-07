8af41b2d44957053d64ee47487cf773fb02c8924
Kevin van der Vlist <kevin@kevinvandervlist.nl>
2012-01-07 15:16:49 +0100

    Backup old data.

--- setup.sh
+++ setup.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then
 	echo "Copy the emacs config to a certain directory."
+	echo "The old emacs config is backed up."
 	echo "Usage: `basename $0` /path/to/dest."
 	exit 1
 fi
 
 DEST=$1
 CWD=$(pwd)
+TMPDIR=$(mktemp -d)
 
 mkdir -p ${DEST}
+
+if [ -f "${DEST}/.emacs" ]; then
+	cp -r ${DEST}/.emacs.d ${TMPDIR}/.emacs
+fi
+
+if [ -d "${DEST}/.emacs.d" ]; then
+	cp -r ${DEST}/.emacs.d ${TMPDIR}/.emacs.d
+fi
 
 cp -r ${CWD}/.emacs.d ${DEST}/.emacs.d
 cp ${CWD}/.emacs ${DEST}/.emacs
 
 echo "Copied files to ${DEST}/."
+echo "Backup is in ${TMPDIR}"
 exit 0
